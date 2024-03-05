f3a131d57dcffa8b2da3983a37b547a834365a96
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-03-05 01:21:44 -0300

    Module 02-aula 06

--- src/typescript.ts
+++ src/typescript.ts
@@ -1,24 +1,30 @@
-let valor: number = 3000; 
+// Tipos Primitivos
+let valor: number = 3000;
 let nome: string = "";
 let isPago: boolean = false;
 let qualquer: any = "";
-qualquer = 22; 
+qualquer = 22;
 
-// Arrays 
-
+// Arrays
 const lista: number[] = [];
 lista.push(13, 22.5, 22, 89, 1.58);
 
-// Tipos Personalizados (Type Alias) 
+// Tipos Personalizados (Type Alias)
 type Transacao = {
-    tipoTransacao: string;
-      data: Date;
-      valor: number;
- }
- 
- const novaTransacao: Transacao = {
-     tipoTransacao: "", 
-         data: new Date(),
-         valor: 0
- }
- 
+    tipoTransacao: TipoTransacao;
+    data: Date;
+    valor: number;
+}
+
+// Enum
+enum TipoTransacao {
+    DEPOSITO = "Depósito",
+    TRANSFERENCIA = "Transferência",
+    PAGAMENTO_BOLETO = "Pagamento de Boleto"
+}
+
+const novaTransacao: Transacao = {
+    tipoTransacao: TipoTransacao.PAGAMENTO_BOLETO,
+    data: new Date(),
+    valor: 0
+}
