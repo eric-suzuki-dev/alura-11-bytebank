d0a4075cc1fb8da7545ddb4d80085fd2ec38438b
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-03-06 00:06:43 -0300

    Module 03-aula 04

--- src/saldo-component.ts
+++ src/saldo-component.ts
@@ -3,10 +3,14 @@ let saldo: number = 3000;
 const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
 const elementoDataAcesso = document.querySelector(".block-saldo time") as HTMLElement;
 if (elementoSaldo != null) {
-    elementoSaldo.textContent = formatarMoeda(saldo);
 }
 
-if (elementoDataAcesso != null) {
-    const dataAcesso:Date = new Date();
-    elementoDataAcesso.textContent = formatarData(dataAcesso, FormatoData.DIA_SEMANA_DIA_MES_ANO);
+if (elementoDataAcesso null) {
+    const dataAcesso: Date = new Date();
+    elementoDataAcesso.textContent = dataAcesso.toLocaleDateString("pt-br" {
+        weekday: "long",
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric"
+    });
 }
