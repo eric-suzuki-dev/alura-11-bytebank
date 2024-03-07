154d6c1970a87735b7e4fc537fec3adef8174723
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-03-06 23:29:43 -0300

    Module 04-aula 03

--- dist/js/components/saldo-component.js
+++ dist/js/components/saldo-component.js
@@ -1,23 +1,20 @@
 import { formatarData, formatarMoeda } from "../utils/formatters.js";
 import { FormatoData } from "../types/FormatoData.js";
 
-const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
-const elementoDataAcesso = document.querySelector(".block-saldo time") as HTMLElement;
-
+const elementoSaldo = document.querySelector(".saldo-valor .valor");
+const elementoDataAcesso = document.querySelector(".block-saldo time");
 if (elementoDataAcesso != null) {
     elementoDataAcesso.textContent = formatarData(Conta.getDataAcesso(), FormatoData.DIA_SEMANA_DIA_MES_ANO);
 }
-
-function renderizarSaldo(): void {
+renderizarSaldo();
+function renderizarSaldo() {
     if (elementoSaldo != null) {
         elementoSaldo.textContent = formatarMoeda(Conta.getSaldo());
     }
 }
-
 const SaldoComponent = {
     atualizar() {
         renderizarSaldo();
     }
-}
-
+};
 export default SaldoComponent;
