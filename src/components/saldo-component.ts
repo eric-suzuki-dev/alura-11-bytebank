0898c495d578573dc799c2294b37260a9b2b6aa2
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-03-06 23:37:21 -0300

    Module 04-aula 04

--- src/components/saldo-component.ts
+++ src/components/saldo-component.ts
@@ -8,6 +8,7 @@ if (elementoDataAcesso != null) {
     elementoDataAcesso.textContent = formatarData(Conta.getDataAcesso(), FormatoData.DIA_SEMANA_DIA_MES_ANO);
 }
 
+renderizarSaldo();
 function renderizarSaldo(): void {
     if (elementoSaldo != null) {
         elementoSaldo.textContent = formatarMoeda(Conta.getSaldo());
